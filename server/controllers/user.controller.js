0ad3527adc7c1e34138a85b0d164527fb2dd2165
Vamsi Dharmavarapu <vd@Vamsis-MacBook-Pro.local>
2017-02-04 13:06:05 +0530

    first and last name update for user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -29,7 +29,10 @@ function get(req, res) {
 function create(req, res, next) {
   const user = new User({
     username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
     mobileNumber: req.body.mobileNumber
+
   });
 
   user.save()
