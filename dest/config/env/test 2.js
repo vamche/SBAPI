8a90f57a25d42b1f7ff19186cead17227aa89db5
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-03-16 12:03:17 +0530

    Assign pending fix

---
+++ dest/config/env/test 2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  env: 'test',
+  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
+  db: 'mongodb://localhost/seasonboy',
+  port: 4040,
+  cloudinary_cloud_name: 'vamche',
+  cloudinary_api_key: '317823213182943',
+  cloudinary_api_secret: '-NB8N77KnUvp4F7Fp1SGXLmhYFA'
+};
+module.exports = exports['default'];
+//# sourceMappingURL=test.js.map
