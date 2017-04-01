e19ddb4dc2357c2b7c9748b96303ce82e6ec5035
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-04-01 15:36:14 +0530

    Dev pointing to Local DB

--- dest/config/env/development.js
+++ dest/config/env/development.js
@@ -7,7 +7,7 @@ exports.default = {
   env: 'development',
   MONGOOSE_DEBUG: true,
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: 'mongodb://sboy:sboy123@ds157258.mlab.com:57258/seasonboy',
+  db: 'mongodb://localhost/seasonboy',
   port: 4040,
   cloudinary_cloud_name: 'vamche',
   cloudinary_api_key: '317823213182943',
